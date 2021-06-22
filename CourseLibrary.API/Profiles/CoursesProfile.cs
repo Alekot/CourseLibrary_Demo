bd496d723ffa2b62774da94870a19b5c0d0497ea
Александар Коцевски <alek.kocevski@hotmail.com>
2021-06-22 16:46:39 +0200

    Adding a courseForUpdateDto model for put action method model binding functionality

--- CourseLibrary.API/Profiles/CoursesProfile.cs
+++ CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -14,6 +14,7 @@ namespace CourseLibrary.API.Profiles
         {
             CreateMap<Course, CourseDto>();
             CreateMap<CourseForCreationDto, Course>();
+            CreateMap<CourseForUpdateDto, Course>();
         }
     }
 }
