f8979da12f5e5fbcaa7f00250bb6c995a96eef0b
Александар Коцевски <alek.kocevski@hotmail.com>
2021-06-22 18:02:37 +0200

    Adding an abstract model class as base class to the course models for creating/updating

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescription(
-        ErrorMessage ="Title must be different from description.")]
-    public class CourseForCreationDto //: IValidatableObject
+    //[CourseTitleMustBeDifferentFromDescription(
+    //    ErrorMessage ="Title must be different from description.")]
+    public class CourseForCreationDto : CourseForManipulationDto //: IValidatableObject
     {
-        [Required(ErrorMessage ="You should fill out a title.")]
-        [MaxLength(100, ErrorMessage ="The title should't have more than 100 characters.")]
-        public string Title { get; set; }
-        [MaxLength(1500, ErrorMessage ="The description shouldn't have more than 1500 characters.")]
-        public string Description { get; set; }
+        //[Required(ErrorMessage ="You should fill out a title.")]
+        //[MaxLength(100, ErrorMessage ="The title should't have more than 100 characters.")]
+        //public string Title { get; set; }
+        //[MaxLength(1500, ErrorMessage ="The description shouldn't have more than 1500 characters.")]
+        //public string Description { get; set; }
 
         //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         //{
